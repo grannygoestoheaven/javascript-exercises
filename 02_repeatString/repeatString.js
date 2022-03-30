98e30447bfb2abaee973e6fbc8dcf2459a1862c9
grannygoestoheaven <myganou@hotmail.com>
2022-03-30 11:14:51 +0200

    repeatString_array_technique

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,23 @@
-const repeatString = function() {
-
+const repeatString = function(string, num)
+{
+    if (num === 0)
+    {
+        return ("");
+    }
+    if (num < 0)
+    {
+        return "ERROR";
+    }
+    let i = 0;
+    string = string.split();
+    while (i < num - 1)
+    {
+        string = string.concat(string[0]);
+        i++;
+    }
+    string = string.toString();
+    string = string.replace(/,/g, '');
+    return string;
 };
 
 // Do not edit below this line
