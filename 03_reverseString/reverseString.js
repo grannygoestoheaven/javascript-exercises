8a31baab3c8089fc151a60c803f3c01aa8727a3f
grannygoestoheaven <myganou@hotmail.com>
2022-03-31 11:53:43 +0200

    reverseString_array_techniaue

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string)
+{
+    string = string.split("");
+    string = string.reverse();
+    string = string.toString();
+    string = string.replace(/,/g, '');
+    return string ;
 };
+
+let string = 'hello';
+console.log(reverseString(string));
 
 // Do not edit below this line
 module.exports = reverseString;
