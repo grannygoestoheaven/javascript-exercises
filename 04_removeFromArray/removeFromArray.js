401e4cba71449cdd31763e950e1e9966235c28f9
grannygoestoheaven <myganou@hotmail.com>
2022-04-16 15:59:31 +0200

    Using filter + imperative code for the second loop

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...values)
+{
+    return array.filter((el) =>
+    {
+        let i = 0;
+        while (i < values.length)
+        {
+            if (el === values[i])
+            {
+                return false;
+            }
+            i++;
+        }
+        return true;
+    });
 };
 
 // Do not edit below this line
