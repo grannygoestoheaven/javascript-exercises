42b7ae3921950dc422799cb96ca10f2e41bf87bd
grannygoestoheaven <myganou@hotmail.com>
2022-04-21 09:34:16 +0200

    sumAll_fixed_console.log_removed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,7 +3,7 @@ const sumAll = function(num1, num2)
     let i = num1;
     let sum = num1;
 
-    if (num1 < 0 || num2 < 0 || typeof num1 !== 'number' || typeof num2 !== 'number')
+    if (num1 < 0 || num2 < 0)
         return "ERROR";
     if (num1 > num2)
     {
@@ -23,8 +23,6 @@ const sumAll = function(num1, num2)
     }
     return sum;
 };
-
-console.log(sumAll(45, 32));
 
 // Do not edit below this line
 module.exports = sumAll;
