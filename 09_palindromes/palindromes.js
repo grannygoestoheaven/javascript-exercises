00649bc954a758efe188c17ffdd4c0effe522aa5
grannygoestoheaven <myganou@hotmail.com>
2022-04-24 14:19:28 +0200

    palindromes_all_to_lowercase_and_no_punctuation_method

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,47 @@
-const palindromes = function () {
+// const palindromes = function (string) {
+//     let reversedString = string;
+//     reversedString = reversedString.split("");
+//     reversedString = reversedString.reverse();
+//     reversedString = reversedString.toString();
+//     reversedString = reversedString.replace(/,/g, '');
 
-};
+    // if (reversedString === string)
+    //     return true;
+    // else
+    //     return false;
+// };
+
+const palindromes = function (string)
+{
+    string = string.split("");
+    let lowString = string.map(function(element)
+    {
+        for (let i = 0; i < element.length; i++)
+        {
+            if (element[i] === '"' || element[i] === '.' || element[i] === '!' || element[i] === ' ')
+            {
+              continue;
+            }
+            return element.toLowerCase();
+        }
+    });
+    lowString = lowString.toString();
+    lowString = lowString.replace(/,/g, '');
+    const reverseString = function(lowString)
+    {
+        lowString = lowString.split("");
+        lowString = lowString.reverse();
+        lowString = lowString.toString();
+        lowString = lowString.replace(/,/g, '');
+        return lowString;
+    }
+    
+    let newString = reverseString(lowString);
+    if (newString === lowString)
+        return true;
+    else
+        return false;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
