1fcdc9355e32da6ca3603f4aab29371400de70f2
grannygoestoheaven <myganou@hotmail.com>
2022-05-13 11:38:37 +0200

    find_the_oldest_yearOfDeath=currentDate_method

---
+++ 12_findTheOldest/comments.js
@@ -0,0 +1,12 @@
+/*
+    - get the age of the person for each object by substracting the year of birth form the year of death in each object
+    - gather the ages in a new array
+    - apply a reduce method on this array to get the oldest person
+    - write the proper conditions to match the given instructions
+
+    - if I have (acc, current), my acc will be the result of one (yearofdeath - yearofbirth) and current will be the result of the next one
+    */
+
+    // let ages = array.map(element => element.yearOfDeath - element.yearOfBirth)
+    // let oldest = ages.reduce((acc, current) => Math.max(acc, current));
+    // return oldest;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,58 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array)
+{
+    let oldest = array.reduce((acc, current) =>
+    {
+      array.map(function(element)
+      {
+        if (!(element.yearOfDeath))
+          element.yearOfDeath = new Date().getFullYear();
+      })
+      array.map(element => element.age = element.yearOfDeath - element.yearOfBirth);
+      if (acc.age < current.age)
+      {
+          return current; // with reduce it means that the returned value will be assigned to acc
+      }
+      return acc;
+    })
+    return oldest;
 };
+
+// const findTheOldest = function(array)
+// {
+//     let i = 0;
+//     let oldest = array.filter(element =>
+//     {
+//         element.age = element.yearOfBirth - element.yearOfDeath;
+//         while (i < array.length)
+//         {
+//             if (element.age[i] < element.age[i + 1])
+//                 return element[i + 1];
+//             i++;
+//             return element[i];
+//         }
+//     });
+//     return oldest;
+// }
+
+// const people = [
+//     {
+//       name: "Carly",
+//       yearOfBirth: 1942,
+//       yearOfDeath: 1970,
+//     },
+//     {
+//       name: "Ray",
+//       yearOfBirth: 1962,
+//       yearOfDeath: 2011,
+//     },
+//     {
+//       name: "Jane",
+//       yearOfBirth: 1912,
+//       yearOfDeath: 1941,
+//     },
+//   ]
+
+  // console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
